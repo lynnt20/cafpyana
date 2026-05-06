284d036d3aad3490c0af2095f3978d7c5bb8e9b0
lynnt20 <lynnt@uchicago.edu>
2026-05-06 08:43:12 -0500

    move to better named config

--- analysis_village/nuecc/configs/nuecc_mc_wgt_ar23p.py
+++ analysis_village/nuecc/configs/nuecc_mc_wgt_ar23p.py
@@ -1,11 +1,8 @@
 from analysis_village.nuecc.makedf.make_nueccdf import * 
 
 DFS =   [make_hdrdf,
-         make_mcnudf_nuecc_sigwgt_ar23p,
          make_nueccdf_mc_wgt_ar23,
          ]
 
 NAMES = ["hdr", 
-         "mcnuecc",
-         "nuecc"
-         ]
+         "nuecc"]
