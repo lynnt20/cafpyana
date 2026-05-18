8a621dafdade66dd4d6f49aa08248772a3287753
lynnt20 <lynnt@uchicago.edu>
2026-05-17 21:35:56 -0500

    add config to get AV nuecc signal only

---
+++ analysis_village/nuecc/configs/nuecc_mc_sig.py
@@ -0,0 +1,11 @@
+from analysis_village.nuecc.makedf.make_nueccdf import * 
+
+GRID_PARAMS = {
+    "memory": "3GB",
+    "cpu":    7,
+    "disk":   "100GB",
+    "lifetime": "1h",
+}
+
+DFS =   [make_mcnudf_nuecc_sig, make_hdrdf, make_potdf_bnb]
+NAMES = ["mcnuecc", "hdr", "pot"]
