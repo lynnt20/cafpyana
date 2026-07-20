ad19e35b26e1d4d3ec87f8007ee09bac88d56f7a
lynnt20 <lynnt@uchicago.edu>
2026-07-20 13:33:03 -0500

    add default grid params

--- analysis_village/nuecc/configs/nuecc_signal.py
+++ analysis_village/nuecc/configs/nuecc_signal.py
@@ -1,4 +1,11 @@
 from analysis_village.nuecc.makedf.make_nueccdf import * 
 
+GRID_PARAMS = {
+    "memory": "3GB",
+    "cpu":    7,
+    "disk":   "100GB",
+    "lifetime": "1h",
+}
+
 DFS =   [make_nuecc_df_data_signal, make_hdrdf, make_potdf_bnb]
 NAMES = ["nuecc", "hdr", "pot"]
