6226e982287cfff5bf3fe752cdccf4b85dee76ce
lynnt20 <lynnt@uchicago.edu>
2026-07-21 10:15:30 -0500

    cleanup

--- makedf/branches.py
+++ makedf/branches.py
@@ -41,17 +41,10 @@ trigger_info_branches = [
 ]
 
 opflashbranches = [
-    "rec.opflashes.onbeamtime",      
-    "rec.opflashes.cryo",            
-    "rec.opflashes.firstpmt",        
     "rec.opflashes.tpc",             
     "rec.opflashes.time",            
-    "rec.opflashes.timewidth",       
-    "rec.opflashes.timemean",        
-    "rec.opflashes.timesd",          
     "rec.opflashes.firsttime",       
     "rec.opflashes.totalpe",         
-    "rec.opflashes.fasttototal",         
 ]
 
 numipotbranches = [
