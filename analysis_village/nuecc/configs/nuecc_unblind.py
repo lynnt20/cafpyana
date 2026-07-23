a6729b48c85127714215ea767d62c2a07353f4bf
lynnt20 <lynnt@uchicago.edu>
2026-07-23 17:10:51 -0500

    full unblind config

---
+++ analysis_village/nuecc/configs/nuecc_unblind.py
@@ -0,0 +1,11 @@
+from analysis_village.nuecc.makedf.make_nueccdf import * 
+
+GRID_PARAMS = {
+    "memory": "3GB",
+    "cpu":    7,
+    "disk":   "100GB",
+    "lifetime": "1h",
+}
+
+DFS =   [make_nueccdf_threshold_data, make_hdrdf, make_potdf_bnb, make_triggerdf, make_intime_opflash]
+NAMES = ["nuecc", "hdr", "pot","trigger", "opflash"]
