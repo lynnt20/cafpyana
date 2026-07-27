bda686188587cb9359269932fef3abf07625be50
lynnt20 <lynnt@uchicago.edu>
2026-07-27 10:55:05 -0500

    make sure all mc configs have nulite

--- analysis_village/nuecc/configs/nuecc_mc.py
+++ analysis_village/nuecc/configs/nuecc_mc.py
@@ -7,5 +7,5 @@ GRID_PARAMS = {
     "lifetime": "1h",
 }
 
-DFS =   [make_nueccdf_mc, make_hdrdf, make_potdf_bnb]
-NAMES = ["nuecc", "hdr", "pot"]
+DFS =   [make_nueccdf_mc, make_hdrdf, make_potdf_bnb, make_mcnulite_df_nuecc]
+NAMES = ["nuecc", "hdr", "pot", "nulite"]
--- analysis_village/nuecc/makedf/make_nueccdf.py
+++ analysis_village/nuecc/makedf/make_nueccdf.py
@@ -88,9 +88,10 @@ def make_mcnulite_df_nuecc(f):
     nu_df = loadbranches(f["recTree"], ["rec.mc.nu.E","rec.mc.nu.pdg"]).rec.mc.nu
     # keep only the maximum energy
     nu_df = nu_df.sort_values("E").groupby(level=[0,1]).nth(-1)
+    nu_df = nu_df.rename(columns={"E": "idxE"})
     names = nu_df.index.names
     df = nu_df.reset_index().merge(rse_df.reset_index(),on='entry').set_index(names)
-    return df 
+    return df
 
 def make_mcnudf_nuecc(f,**args):
     mcdf = make_mcnudf(f,**args)
